a696565f42ca1208995fb96e5174231b9a371f66
DESKTOP-NIKITA\Nikita Zholnerovich <nikita.zholnerovich21@gmail.com>
2026-02-25 19:44:13 +0300

    resolve conflicts with Osama

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi I'm Osama");
 Console.WriteLine("I love to Learn");
-
+Console.WriteLine("Hello, my name's Nikita. I like reading books.");
