540cf9d93f75e9bd3a00d0f4b053c91c25e9e0cc
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-02-25 20:26:07 +0300

    Fix after merge

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi I'm Pasha");
+Console.WriteLine("Hi I'm Pasha I write a book");
 Console.WriteLine("Hi I'm Alena");
+Console.WriteLine(("Hi I am Nikolai. Hello!"));
+Console.WriteLine("I want to work programmist!:");
+Console.WriteLine("Hi, I'm Dmitry. I like Videogames but suck at programming!");
+Console.WriteLine("Hello, my name is Ilya");
+Console.WriteLine("Hi I'm Stas - C++ Developer");
+Console.WriteLine("Hi I'm Valentin");
+Console.WriteLine("Hi I'm Osama");
+Console.WriteLine("I love to Learn");
+Console.WriteLine("Hello, I'm Alexander. My hobby is a waste of time");
+Console.WriteLine("Hi I'm Denis");
+Console.WriteLine("Hello, my name's Nikita. I like reading books.");
+Console.WriteLine("Hello, my name is Ira, i love my cats");
+Console.WriteLine("Hi I'm Sonya");
+Console.WriteLine("I love dancing");
+Console.WriteLine("I am trying again");
+Console.WriteLine("Igor, I like to drive a car.");
