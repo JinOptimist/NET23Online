b9520b79193521b114c59c09250d0966b80c880b
MikalaiTarasenka <51235834+MikalaiTarasenka@users.noreply.github.com>
2026-02-25 19:24:14 +0300

    First commit. I want to be programmist.

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi I'm Pasha");
+Console.WriteLine(("Hi I am Nikolai. Hello!");
+Console.WriteLine("I want to work programmist!:");
