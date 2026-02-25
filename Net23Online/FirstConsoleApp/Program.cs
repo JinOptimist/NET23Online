6ffae4bb7b694b14c62bbddb89f7dba412c5cf8e
Ketuse <catwithshotgunster34@gmail.com>
2026-02-25 19:43:00 +0300

    Пожалуйста отправься

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hi I'm Osama");
 Console.WriteLine("I love to Learn");
+Console.WriteLine("Hello, I'm Alexander. My hobby is a waste of time");
 
