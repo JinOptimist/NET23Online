598fa3374396755fb8cc68da2fd5a5dd199e62bd
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-02-25 20:13:27 +0300

    Fix task 2

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,9 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hi I'm Pasha");
+Console.WriteLine("Hi I'm Pasha write a book");
 Console.WriteLine(("Hi I am Nikolai. Hello!"));
 Console.WriteLine("I want to work programmist!:");
-Console.WriteLine("Hi I'm Pasha");
-Console.WriteLine("Hi I'm Pasha I write a book");
 Console.WriteLine("Hi, I'm Dmitry. I like Videogames but suck at programming!");
 Console.WriteLine("Hello, my name is Ilya");
 Console.WriteLine("Hi I'm Stas - C++ Developer");
@@ -13,13 +11,8 @@ Console.WriteLine("I love to Learn");
 Console.WriteLine("Hello, I'm Alexander. My hobby is a waste of time");
 Console.WriteLine("Hi I'm Denis");
 Console.WriteLine("Hello, my name's Nikita. I like reading books.");
-
-Console.WriteLine("Hi I'm Pasha");
 Console.WriteLine("Hello, my name is Ira, i love my cats");
-
 Console.WriteLine("Hi I'm Sonya");
 Console.WriteLine("I love dancing");
 Console.WriteLine("I am trying again");
-
-Console.WriteLine("Hi I'm Pasha");
 Console.WriteLine("Igor, I like to drive a car.");
