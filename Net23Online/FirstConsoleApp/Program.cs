0a748b235e33b415435c456686feb147f706533c
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-02-26 19:46:00 +0300

    Small changes

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -16,3 +16,28 @@ Console.WriteLine("Hello, my name's Nikita. I like reading books.");
 
 Console.WriteLine("Hi I'm Sonya");
 Console.WriteLine("I love dancing");
+
+Console.WriteLine("Please, tell me your name");
+string name = Console.ReadLine();
+Console.WriteLine("Now tell me your family");
+string family = Console.ReadLine();
+Console.WriteLine($"Hello {family} {name}! I suggest you play a game. Guess a number.");
+Console.ReadLine();
+Console.WriteLine("Multiply it by 2.");
+Console.ReadLine();
+Console.WriteLine("Add 8 to the result.");
+Console.ReadLine();
+Console.WriteLine("Divide the resulting number by 2.");
+Console.ReadLine();
+Console.WriteLine("Subtract the number you initially thought of from the result.");
+Console.ReadLine();
+Console.WriteLine("I already know you got 4. If so, then write something, otherwise just press \"Enter\".");
+string answer = Console.ReadLine();
+if (answer == "")
+{
+    Console.WriteLine("You made a mistake in your calculations or are trying to deceive me.");
+}
+else
+{
+    Console.WriteLine("In fact, everything is simple. It's a math trick and the answer is always 4.");
+}
