cafcb81e3c86198fc652b53a3ba1aaec0789c68c
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-02-26 22:19:57 +0300

    Rework homework

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -41,3 +41,33 @@ else
 {
     Console.WriteLine("In fact, everything is simple. It's a math trick and the answer is always 4.");
 }
+    Console.WriteLine($"You are lier");
+}
+
+
+Console.WriteLine($"Hi {userName} cool you are {age} is old");
+
+Console.WriteLine("Please, tell me your name");
+string name = Console.ReadLine();
+Console.WriteLine("Now tell me your family");
+string family = Console.ReadLine();
+Console.Write($"Hello {family} {name}! I suggest you play a game. Guess a number. I think it is 3");
+Console.ReadLine();
+Console.Write("Multiply it by 2. It is 6");
+Console.ReadLine();
+Console.Write("Add 8 to the result. It is 14");
+Console.ReadLine();
+Console.Write("Divide the resulting number by 2. It is 7");
+Console.ReadLine();
+Console.Write("Subtract the number you initially thought of from the result. It is 4");
+Console.ReadLine();
+Console.Write("I know you got 4. What does it mean that I guessed the number you were thinking of? If so, then write something, otherwise just press \"Enter\".");
+string answer = Console.ReadLine();
+if (answer == "")
+{
+    Console.WriteLine("You made a mistake in your calculations or are trying to deceive me.");
+}
+else
+{
+    Console.WriteLine("In fact, everything is simple. It's a math trick and the answer is always 4.");
+}
