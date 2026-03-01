bd31a5980a3536b6be6021ab407e9c7e7c91da3c
Ира Лаврусик <iriplay1995@gmail.com>
2026-03-01 16:37:32 +0300

    testing welcome_homework

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -44,7 +44,7 @@
 
 //Console.WriteLine(name);
 
-
+/*
 Console.WriteLine("What is you name?");
 var userName = Console.ReadLine(); // read from console
 
@@ -58,6 +58,63 @@ if (age < 3)
     Console.WriteLine($"You are lier");
 }
 
+Console.WriteLine($"Hi {userName} cool you are {age} is old"); */
 
-Console.WriteLine($"Hi {userName} cool you are {age} is old");
+            var name = "irina";
+            var YouAge = 18;
+            var MyAge = 24;
+            var BirthdayYear = DateTime.Now.Year;
+            var play_sport = "yes";
+            var vid_sporta = "tenis";
+            var let_v_sporte = 1;
+            var raznica_vozrast = 1;
+
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What year were you born?");
+            BirthdayYear = Convert.ToInt32(Console.ReadLine());
+            YouAge = DateTime.Now.Year - BirthdayYear;
+            Console.WriteLine("Do you play sports?");
+            play_sport = Console.ReadLine();
+
+            if (play_sport == "yes" || play_sport == "Yes")
+            {
+                Console.WriteLine("What kind of sport do you play?");
+                vid_sporta = Console.ReadLine();
+                Console.WriteLine("How many years have you been practicing?");
+                let_v_sporte = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"Hello, {name}, my name is Ira! Nice to meet you! You {YouAge}?");
+
+            if (YouAge > MyAge)
+            {
+                raznica_vozrast = YouAge - MyAge;
+                Console.WriteLine($"You're {raznica_vozrast} years older than me. This is a wonderful age!");
+            }
+            else
+            {
+                raznica_vozrast = Math.Abs(YouAge - MyAge);
+                Console.WriteLine($"You're {raznica_vozrast} years younger than me. This is a wonderful age!");
+            }
+
+
+            if (play_sport == "yes" || play_sport == "Yes")
+            {
+                Console.WriteLine($"It's great that you've been {vid_sporta} this for {let_v_sporte} years. That's quite respectable!");
+            }
+            else if (play_sport == "no" || play_sport == "No")
+            {
+                Console.WriteLine("No hobbies? No problem, maybe you just don't have any free time.");
+            }
+            else
+            {
+                Console.WriteLine("I think we had a misunderstanding on the last question.");
+            }
+        
 
