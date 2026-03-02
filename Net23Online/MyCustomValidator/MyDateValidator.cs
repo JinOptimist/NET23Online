a44cd03b7fb16089626309c6f26189fd71250ad2
Qdesnitsa <lenkasid@tut.by>
2026-03-02 19:44:02 +0300

    add my_custom_validator

---
+++ Net23Online/MyCustomValidator/MyDateValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace MyCustomValidator
+{
+    public static class MyDateValidator
+    {
+        /// <summary>
+        /// checks if the provided year of birth is a valid integer and falls within the specified minimum and maximum year range.
+        /// </summary>
+        /// <param name="year"></param> some string that needs to be checked for min and max values
+        /// <param name="minYear"></param> minimum year that is allowed for the year of birth
+        /// <param name="maxYear"></param> maximum year that is allowed for the year of birth
+        /// <returns></returns>
+        public static bool IsYearOfBirthValid(string year, int minYear, int maxYear)
+        {
+            bool isValid = int.TryParse(year, out int paresedYear);
+            return isValid && paresedYear >= minYear && paresedYear <= maxYear;
+        }
+    }
+}
---
+++ Net23Online/MyCustomValidator/MyStringValidator.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyCustomValidator
+{
+
+    public static class MyStringValidator
+    {
+        /// <summary>
+        /// checks if the provided string contains only Latin letters (both uppercase and lowercase) and does not contain any other characters, such as digits, spaces, or special characters.
+        /// </summary>
+        /// <param name="text"></param> some string that needs to be checked for containing only Latin letters
+        /// <returns></returns>
+        public static bool DoesStringContainLatinLettersOnly(string text)
+        {
+            return Regex.IsMatch(text, @"^[a-zA-Z]+$");
+        }
+
+        /// <summary>
+        /// checks if the length of the provided string is greater than the specified minimum length and less than the specified maximum length. It returns true if the string length is valid according to these criteria, and false otherwise.
+        /// </summary>
+        /// <param name="someString"></param> some string that needs to be checked for length
+        /// <param name="minNameLength"></param> minimum length that is allowed for the string
+        /// <param name="maxNameLength"></param> maximum length that is allowed for the string
+        /// <returns></returns>
+        public static bool IsStringLengthValid(string someString, int minNameLength, int maxNameLength)
+        {
+            return someString.Length > minNameLength && someString.Length < maxNameLength;
+        }
+    }
+}
