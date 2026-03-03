a390ce94ee56eb27c0805a67d86f2b75ca6355b0
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-03-03 19:17:00 +0300

    add new Method from AlexanderKartuz

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,5 +1,6 @@
-﻿GuessTheNumberFromLvou(); // You can change it
+﻿//GuessTheNumberFromLvou(); // You can change it
 
+GuessTheNumberFromALexanderKartuz();
 static int GetNumberFromConsole(string messageForUser)
 {
     var isThisANumberForMax = false;
@@ -53,5 +54,63 @@ static void GuessTheNumberFromLvou()
     } while (!isGuessAreRight); // isGuessAreRight == false
 
     Console.WriteLine("The end");
+}
+
+static void GuessTheNumberFromALexanderKartuz()
+{
+    var minValue = GetNumberFromConsole("Enter a min value.");
+    var maxValue = 0;
+    do
+    {
+        maxValue = GetNumberFromConsole("Enter a max value");
+        if (maxValue <= minValue)
+        {
+            Console.WriteLine($"Error! Max value must be bigger then min value ({minValue}). Try enter max value again.");
+        }
+
+    } while (maxValue <= minValue);
+
+    var random = new Random();
+    var theNumber = random.Next(minValue, maxValue); // Random [minValue, maxValue]
+        
+    Console.WriteLine("Lets start a game.");
+    Console.WriteLine($"Try to guess the number between {minValue} and {maxValue}.");
+
+    var tries = 0;
+    var maxTry = 10;
+    var isGuessAreRight = false;
+
+    do
+    {
+        tries++;
+        Console.WriteLine($"Try {tries}/{maxTry}. You have {maxTry-tries+1} tries.");
+        var guess = GetNumberFromConsole("Enter your guess.");
+
+        if (guess < minValue || guess > maxValue)
+        {
+            Console.WriteLine($"The number is between {minValue} and {maxValue}! Try again.\n");
+            continue;
+        }
+        if (guess < theNumber)
+        {
+            Console.WriteLine("My number is bigger.\n");
+        }
+        if (guess > theNumber)
+        {
+            Console.WriteLine("My number is less.\n");
+        }
+        if (guess == theNumber)
+        {
+            Console.WriteLine($"You are win. You used {tries} tries.\n");
+        }
+
+        isGuessAreRight = guess == theNumber;
+
+    } while (!(isGuessAreRight || tries ==maxTry));
+    if (!isGuessAreRight)
+    {
+        Console.WriteLine($"Game over!!! You have used all {maxTry} tries.\nThe number was: {theNumber}.");
+        
+    }
 }
 
