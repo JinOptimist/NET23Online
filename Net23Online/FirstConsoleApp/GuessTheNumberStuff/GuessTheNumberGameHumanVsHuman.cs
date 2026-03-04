51e290c930d41666c985e582b1446d5369f5ee77
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-03-04 21:46:08 +0300

    Craete a lot of classes

---
+++ Net23Online/FirstConsoleApp/GuessTheNumberStuff/GuessTheNumberGameHumanVsHuman.cs
@@ -0,0 +1,22 @@
+﻿namespace FirstConsoleApp.GuessTheNumberStuff
+{
+    public class GuessTheNumberGameHumanVsHuman : BaseGuessTheNumberGame
+    {
+        protected override void ConigurateGameRule()
+        {
+            _rule = new GameRule();
+
+            var random = new Random();
+
+            _rule.MinValue = GetNumberFromConsole("Enter a min value");
+
+            _rule.MaxValue = GetNumberFromConsole("Enter a max value");
+
+            _rule.TheNumber = random.Next(1, 10); // [1, 10]
+
+            _rule.Attempt = 0;
+
+            _rule.MaxAttempt = CalculateMaxAttempt();
+        }
+    }
+}
---
+++ Net23Online/FirstConsoleApp/Users/YangWoman.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstConsoleApp.Users
+{
+    public class YangWoman : Woman
+    {
+        public YangWoman(string name) : base(name)
+        {
+
+        }
+
+        public override void SayMyName()
+        {
+            Console.WriteLine($"I'm to yang to name");
+        }
+    }
+}
