670693f915a284755591590b0d3f93bece2a5e35
Соня Федоренко <sonyfedorenko@gmail.com>
2026-03-06 00:56:32 +0300

    Add bool CanPlaceShip in class Field.cs

--- Net23Online/FirstConsoleApp/SeaBattleHumanVsBot/Field.cs
+++ Net23Online/FirstConsoleApp/SeaBattleHumanVsBot/Field.cs
@@ -23,7 +23,81 @@ public class Field
 
     //public Cell GetCell(int row, int col)
 
-   // bool CanPlaceShip(int row, int col, int size, bool horizontal)
+    //координата - верхняя\левая палуба корабля
+    bool CanPlaceShip(int row, int col, int size, bool horizontal, Field field)
+    {
+        if (!horizontal)
+        {
+            if (col > 10  || col < 1) //если выходит за границы столбцов
+            {
+                return false;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (size == 1+i && row > 10-i && row < 1)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            if (row > 10 || row < 1) //если выходит за границы строк
+            {
+                return false;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (size == 1+i && col > 10-i && col < 1)
+                {
+                    return false;
+                }
+            }
+        }
+
+        //HashSet<Cell> emptyCells = new HashSet<Cell>();
+        for (int c = 0; c < size; c++) //cделаем с каждой клеткой
+        {
+            var rows = 0;
+            var cols = 0;
+            for (int i = -1; i <= 1; i++) //в каждой ее строке
+            {
+                for (int j = -1; j <= 1; j++) // в каждом столбце
+                {
+                    
+                    if (!horizontal)
+                    {
+                        rows = row + i;
+                        cols = col + j + c;
+                    }
+                    else
+                    {
+                        rows = row + i + c;
+                        cols = col + j;
+                    }
+                    
+
+                    if (rows < 1 ||
+                        rows > 10 ||
+                        cols < 1 ||
+                        cols > 10)
+                    {
+                        continue;
+                    }
+                    
+                    Cell cell =  field.Cells[rows-1, cols-1];
+
+                    if (cell.State != CellState.Empty)
+                    { 
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
    //void PlaceShip(Ship ship)
    
