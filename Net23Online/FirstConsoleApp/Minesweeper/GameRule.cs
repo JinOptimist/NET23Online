08208bd25dd8701ee9b30ca37ca5ca06c8a29111
Geliusan <xa2@tut.by>
2026-03-09 15:55:07 +0300

    Added basic methods for playing minesweeper

---
+++ Net23Online/FirstConsoleApp/Minesweeper/GameRule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp.Minesweeper
+{
+    public class GameRule
+    {
+        public int FieldHeight { get; set; }
+        public int FieldWidth { get; set; }
+        public int NumberOfBombs { get; set; }
+
+    }
+}
--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using FirstConsoleApp.GuessTheNumberStuff;
+using FirstConsoleApp.Minesweeper;
 
-var botGame = new GuessTheNumberGameHumanVsBot();
-botGame.Play();
+//var botGame = new GuessTheNumberGameHumanVsBot();
+//botGame.Play();
+
+var miner = new HardMode();
+miner.PlayMinesweeper();
