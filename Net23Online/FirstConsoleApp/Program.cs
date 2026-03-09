14c56041189cfbc702744ed19a22ce25edf81c88
Yato <Yato@YATO-PC>
2026-03-09 19:56:26 +0300

    Merge with main

--- Net23Online/FirstConsoleApp/Program.cs
+++ Net23Online/FirstConsoleApp/Program.cs
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+﻿using FirstConsoleApp.BullsAndCowsGameBySleepaidyAndYato;
+using FirstConsoleApp.GuessTheNumberStuff;
+
+//var botGame = new GuessTheNumberGameHumanVsBot();
+//botGame.Play();
+
+var botPlayer = new BotPlayer();
+var botGame = new BullsAndCowsGame(botPlayer);
+botGame.Play();
+=======
 using FirstConsoleApp.GuessTheNumberStuff;
 using FirstConsoleApp.Minesweeper;
 
@@ -13,3 +24,4 @@ var botGame = new GuessTheNumberGameHumanVsBot();
 
 var sokoban = new SokobanHumanVSBot();
 sokoban.Play();
+>>>>>>> main
