0aa3012f282f9be731717db95960f92ecabdb02d
Денис Кибицкий <aidns410@gmail.com>
2026-03-10 11:40:54 +0300

    add a summary

--- Net23Online/FirstConsoleApp/MazeStuff/Cells/SpeedPotion.cs
+++ Net23Online/FirstConsoleApp/MazeStuff/Cells/SpeedPotion.cs
@@ -2,6 +2,9 @@
 
 namespace FirstConsoleApp.MazeStuff.Cells
 {
+    /// <summary>
+    /// speed potion allows: 1. If the game environment moves in turns, then the potion allows you to move twice. 2. If the game environment is static, the potion allows you to pass through 2 cells, for example, bypassing traps.
+    /// </summary>
     internal class SpeedPotion : BaseCell
     {
         public SpeedPotion(Maze maze) : base(maze)
