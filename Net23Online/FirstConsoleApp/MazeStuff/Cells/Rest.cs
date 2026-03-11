7b2ab1597eeee0d8f30ca8eb89ef1c8d54b2b91e
Igor <User@DESKTOP-GUGUSI8>
2026-03-11 14:09:52 +0300

    Rest

---
+++ Net23Online/FirstConsoleApp/MazeStuff/Cells/Rest.cs
@@ -0,0 +1,35 @@
+﻿using FirstConsoleApp.MazeStuff.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp.MazeStuff.Cells
+{
+    internal class Rest : BaseCell
+    {
+
+
+        public Rest(Maze maze) : base(maze)
+        {
+        }
+
+        public override char Symbol => 'Q';
+
+        public override bool Interaction(BaseCharacter character)
+        {
+            character.Hp++;
+            Maze.Cells.Remove(this);
+            var ground = new Ground(Maze)
+            {
+                X = X,
+                Y = Y,
+            };
+            Maze.Cells.Add(ground);
+
+            return true;
+        }
+
+    }   
+}
