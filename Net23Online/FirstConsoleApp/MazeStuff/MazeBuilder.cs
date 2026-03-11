a66cf1d3977b934b6ff957d2adac57be36247a48
Nikita Zholnerovich <nikita.zholnerovich21@gmail.com>
2026-03-11 11:51:39 +0300

    issue 110: Add portal class, drawing method for portals

--- Net23Online/FirstConsoleApp/MazeStuff/MazeBuilder.cs
+++ Net23Online/FirstConsoleApp/MazeStuff/MazeBuilder.cs
@@ -1,5 +1,4 @@
-﻿
-using FirstConsoleApp.MazeStuff.Cells;
+﻿using FirstConsoleApp.MazeStuff.Cells;
 
 namespace FirstConsoleApp.MazeStuff
 {
@@ -18,6 +17,7 @@ namespace FirstConsoleApp.MazeStuff
             GenerateWall();
             GenerateGround();// Genrate path
             GenerateCoins();
+            GeneratePortals();
             // Generate other cells
 
             return _maze;
@@ -75,6 +75,32 @@ namespace FirstConsoleApp.MazeStuff
                         Y = y
                     };
                     _maze.Cells.Add(wall);
+                }
+            }
+        }
+
+        private void GeneratePortals()
+        {
+            var groundCells = _maze.Cells
+                .Where(c => c.GetType() == typeof(Ground))
+                .ToList();
+
+            for (var y = 0; y < _maze.Height; y++)
+            {
+                for (var x = 0; x < _maze.Width; x++)
+                {
+                    var currentCell = groundCells.FirstOrDefault(c => c.X == x && c.Y == y);
+
+                    if (currentCell != null && x % 5 == 0 )
+                    {
+                        var portal = new Portal(_maze)
+                        {
+                            X = x,
+                            Y = y,
+                        };
+
+                        ReplaceCell(portal);
+                    }
                 }
             }
         }
