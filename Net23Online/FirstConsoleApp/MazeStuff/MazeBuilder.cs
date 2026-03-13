08f5bcbdb34d9bd9e6d47512a743c64ea19b12b3
Денис Кибицкий <aidns410@gmail.com>
2026-03-13 19:51:11 +0300

    Update the speed potion

--- Net23Online/FirstConsoleApp/MazeStuff/MazeBuilder.cs
+++ Net23Online/FirstConsoleApp/MazeStuff/MazeBuilder.cs
@@ -34,6 +34,7 @@ namespace FirstConsoleApp.MazeStuff
             GenerateMimics();
             // Generate other cells
             GenerateIce();
+            GenerateSpeedPotions();
 
             return _maze;
         }
@@ -266,5 +267,25 @@ namespace FirstConsoleApp.MazeStuff
                 ReplaceCell(ice);
             }
         }
+            private void GenerateSpeedPotions(int maxSpeedPotionsCount = 3)
+        {
+            var deadends = _maze
+                .Surface
+                .Where(x => x is Ground)
+                .Where(x => GetNearCells<Wall>(x).Count() == 3)
+                .ToList();
+
+            for (int i = 0; i < maxSpeedPotionsCount; i++)
+            {
+                var deadend = deadends[i];
+                var speedPotion = new SpeedPotions(_maze)
+                {
+                    X = deadend.X,
+                    Y = deadend.Y,
+                };
+                ReplaceCell(speedPotion);
+            }
+        }
     }
 }
+
