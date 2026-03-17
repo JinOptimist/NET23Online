87ee2292fb64554ab1e36a3bc19cc4c8518b2e3a
Yato <Yato@YATO-PC>
2026-03-17 22:31:27 +0300

    Refactor Shopkeeper

---
+++ Net23Online/FirstConsoleApp/MazeStuff/Cells/Shopkeeper/ShopMenuBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp.MazeStuff.Cells.Shopkeeper
+{
+    internal class ShopMenuBuilder
+    {
+        public ShopMenuBuilder(List<string>)
+        {
+
+        }
+    }
+}
---
+++ Net23Online/FirstConsoleApp/MazeStuff/Cells/Shopkeeper/Shopkeeper.cs
@@ -0,0 +1,59 @@
+﻿using firstconsoleapp.mazestuff.characters;
+using FirstConsoleApp.MazeStuff.Characters;
+using system;
+using system.collections.generic;
+using system.linq;
+using system.text;
+using system.threading.tasks;
+
+namespace FirstConsoleApp.MazeStuff.Cells.Shopkeeper
+{
+    internal class Shopkeeper : BaseCell
+    {
+        private BaseCharacter _character;
+        private List<string> _goods { get; set; }
+        private Random _random;
+        private bool _wantToTrade;
+        public Shopkeeper(Maze maze, Random random) : base(maze)
+        {
+            _random = random;
+        }
+        public override char Symbol => '$';
+        public override bool Interaction(BaseCharacter character)
+        {
+            Maze.EventHistory.Add("Shopkeeper here!");
+            var ShopController = new ShopController(this);
+            ShopController.StartShopMenu();
+
+        }
+
+        private class shopdrawer
+        {
+            private char _cursormenu = '<';
+            public int cursorposition { get; set; }
+            public shopdrawer()
+            {
+                cursorposition = 0;
+            }
+
+            public void draw(shopkeeper shopkeeper)
+            {
+                console.clear();
+                for (int wareindex = 0; wareindex < shopkeeper.wares.count; wareindex++)
+                {
+                    console.write($"{wareindex}. {shopkeeper.wares[wareindex]}");
+                    if (wareindex == cursorposition)
+                    {
+                        console.writeline($" {_cursormenu}");
+                    }
+                    else
+                    {
+                        console.writeline();
+                    }
+                }
+            }
+        }
+
+
+    }
+}
