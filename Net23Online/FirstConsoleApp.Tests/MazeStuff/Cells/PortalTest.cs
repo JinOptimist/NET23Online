667a1cf27b402faeeec50bb98c2c5b73e1c6e60e
Соня Федоренко <sonyfedorenko@gmail.com>
2026-03-23 18:00:19 +0300

    Create more tests

--- Net23Online/FirstConsoleApp.Tests/MazeStuff/Cells/PortalTest.cs
+++ Net23Online/FirstConsoleApp.Tests/MazeStuff/Cells/PortalTest.cs
@@ -33,8 +33,18 @@ public class PortalTest
         _baseCharacterMock
             .SetupProperty(y => y.Y);
         
+        
+        /*Как проверять Readkey?
+         * я столкнулась с проблемой, что в Portal.Interaction был вызов Console.ReadKey который нельзя замокать.
+         *Я создала отдельный интерфейс IInputReader и класс ConsoleInputReader
+         *В классе Portal добавила поле _inputReader и передачу через конструктор — теперь
+         *портал не создаёт ввод сам, а получает его снаружи.
+         *В тесте передаём мок, в игре передаём ConsoleInputReader
+         */
         _inputReaderMock  = new Mock<IInputReader>();
         var inputReader = _inputReaderMock.Object;
+        _inputReaderMock
+            .Setup(x => x.ReadKey()).Returns(ConsoleKey.Y);
 
         _portal = new Portal(maze, inputReader);
         _linkedPortal = new Portal(maze, inputReader);
@@ -51,18 +61,7 @@ public class PortalTest
         _linkedPortal.X = xNew;
         _linkedPortal.Y = yNew;
         
-        _inputReaderMock
-            .Setup(x => x.ReadKey()).Returns(ConsoleKey.Y);
-        
         //Act
-         
-        /*Как проверять Readkey?
-         * я столкнулась с проблемой, что в Portal.Interaction был вызов Console.ReadKey который нельзя замокать. 
-         *Я создала отдельный интерфейс IInputReader и класс ConsoleInputReader 
-         *В классе Portal добавила поле _inputReader и передачу через конструктор — теперь 
-         *портал не создаёт ввод сам, а получает его снаружи. 
-         *В тесте передаём мок, в игре передаём ConsoleInputReader
-        */
         var baseCharacter = _baseCharacterMock.Object;
         _portal.Interaction(baseCharacter);
         
@@ -70,5 +69,73 @@ public class PortalTest
         Assert.That(baseCharacter.X, Is.EqualTo(xNew));
         Assert.That(baseCharacter.Y, Is.EqualTo(yNew));
         
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Interaction_AddToEventHistory(bool usePortal)
+    {
+        //Prepare
+        if (!usePortal)
+        {
+            _inputReaderMock
+                .Setup(x => x.ReadKey()).Returns(ConsoleKey.N);
+        }
+        //Act
+        _portal.Interaction(_baseCharacterMock.Object);
+        
+        //Assert
+        _mazeMock.Verify(x => x.EventHistory.Add(It.IsAny<string>()), Times.Once());
+    }
+
+    [Test]
+    public void Interaction_CanTeleport()
+    {
+        //Prepare
+        
+        //Act
+        var result = _portal.Interaction(_baseCharacterMock.Object);
+        
+        //Assert
+        Assert.That(result, Is.False);
+    }
+    
+    [Test]
+    public void Interaction_CanIgnoreTeleport()
+    {
+        //Prepare
+        _inputReaderMock
+            .Setup(x => x.ReadKey()).Returns(ConsoleKey.N);
+        
+        //Act
+        var result = _portal.Interaction(_baseCharacterMock.Object);
+        
+        //Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    [TestCase(true)]
+    [TestCase(false)]
+    public void Iteraction_ReplacedByGroundIfSingleUse(bool isSingleUse)
+    {
+        //Prepare
+        _portal.IsSingleUse = isSingleUse;
+        
+        //Act
+        _portal.Interaction(_baseCharacterMock.Object);
+        
+        //Assert
+        if (isSingleUse)
+        {
+            _mazeMock.Verify(x => x.Surface.Remove(It.IsAny<Portal>()), Times.Exactly(2));
+            _mazeMock.Verify(x => x.Surface.Add(It.IsAny<Ground>()), Times.Exactly(2));
+        }
+        else
+        {
+            _mazeMock.Verify(x => x.Surface.Remove(It.IsAny<Portal>()), Times.Never());
+            _mazeMock.Verify(x => x.Surface.Add(It.IsAny<Ground>()), Times.Never());
+        }
     }
 }
