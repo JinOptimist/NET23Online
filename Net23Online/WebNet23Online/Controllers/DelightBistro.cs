e9985c3d0d5c65f338ca10f4f40a2af084eac3e0
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-03-31 20:51:06 +0300

    Add DelightBistro/Index page

---
+++ Net23Online/WebNet23Online/Controllers/DelightBistro.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNet23Online.Controllers
+{
+    public class DelightBistroController : Controller
+    {
+        //          /DelightBistro/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
