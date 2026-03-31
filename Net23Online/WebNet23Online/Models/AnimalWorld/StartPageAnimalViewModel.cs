51aa24b12f2ad7894f6e6f65f5d536fcf9493ad8
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-03-31 13:41:15 +0300

    Old page in new Project.

---
+++ Net23Online/WebNet23Online/Models/AnimalWorld/StartPageAnimalViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebNet23Online.Models.AnimalWorld
+{
+    public class StartPageAnimalViewModel
+    {
+        public StartPageAnimalViewModel(string name, string briefDescription)
+        {
+            Name = name;
+            BriefDescription = briefDescription;
+        }
+
+        public string Name { get; set; }
+
+        public string BriefDescription { get; set; }
+    }
+}
