5551313e3d2314c3c0f616f80fecf1878804737c
Osama-arch <Osamarchitectinfo@gmail.com>
2026-03-31 23:12:13 +0300

    Add Little Lemon Page to webNet23Online

---
+++ Net23Online/WebNet23Online/Views/LittleLemon/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebNet23Online.Views.LittleLemon
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
