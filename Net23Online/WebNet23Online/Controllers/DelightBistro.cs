7ecc4a89cb48a7091fafffc50cc59e912cc0ae77
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-03-31 23:58:18 +0300

    Add FoodItem without realization

--- Net23Online/WebNet23Online/Controllers/DelightBistro.cs
+++ Net23Online/WebNet23Online/Controllers/DelightBistro.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebNet23Online.Models.DelightBistro;
 
 namespace WebNet23Online.Controllers
 {
     public class DelightBistroController : Controller
     {
         //          /DelightBistro/Index
-        public IActionResult Index()
+        public IActionResult Index(string menuType)
         {
-            return View();
+
+            var viewModel = new MenuTypeViewModel() { };
+            switch (menuType)
+            {
+                case "soups":
+                    viewModel.MenuType = "Супы";
+                    break;
+                case "hot":
+                    viewModel.MenuType = "Горячее";
+                    break;
+                case "salads":
+                    viewModel.MenuType = "Салаты";
+                    break;
+                    // Нужен ли default?
+            }
+
+            return View(viewModel);
         }
     }
 }
