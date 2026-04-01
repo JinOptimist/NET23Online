472b3c3588158aaa8938e40448f0102375e2fc38
Yato <Yato@YATO-PC>
2026-04-01 19:51:29 +0300

    Refactor

--- Net23Online/WebNet23Online/Controllers/EventWorkshopController.cs
+++ Net23Online/WebNet23Online/Controllers/EventWorkshopController.cs
@@ -18,54 +18,34 @@ namespace WebNet23Online.Controllers
             {
                 case "Creation":
                     {
-                        isCreation = true; 
+                        selectedViewModels?.AddRange(GetCreationViewModels());
                         break;
                     }
                 case "Sport":
                     {
-                        isSport = true;
+                        selectedViewModels?.AddRange(GetSportViewModels());
                         break;
                     }
                 case "Games":
                     {
-                        isGame = true;
+                        selectedViewModels?.AddRange(GetGameViewModels());
                         break;
                     }
                 default:
                     {
-                        isAllType = true;
+                        selectedViewModels = GetCreationViewModels()
+                            .Concat(GetSportViewModels())
+                            .Concat(GetGameViewModels())
+                            .ToList();
                         break;
                     }
-            }
-
-            if (isAllType)
-            {
-                selectedViewModels = GetHobbyViewModels()
-                    .Concat(GetSportViewModels())
-                    .Concat(GetGameViewModels())
-                    .ToList();
-            }
-            else
-            {
-                if (isCreation)
-                {
-                    selectedViewModels?.AddRange(GetHobbyViewModels());
-                }
-                if (isSport)
-                {
-                    selectedViewModels?.AddRange(GetSportViewModels());
-                }
-                if (isGame)
-                {
-                    selectedViewModels?.AddRange(GetGameViewModels());
-                }
             }
             selectedViewModels = selectedViewModels?.ToList();
 
             return View(selectedViewModels);
         }
 
-        private List<EventWorkshopEventInfoViewModel> GetHobbyViewModels()
+        private List<EventWorkshopEventInfoViewModel> GetCreationViewModels()
         {
             var hobbyViewModels = new List<EventWorkshopEventInfoViewModel>();
             hobbyViewModels.Add(new EventWorkshopEventInfoViewModel
@@ -73,7 +53,8 @@ namespace WebNet23Online.Controllers
                 Url = "/images/event-workshop/creation/creation1.jpg",
                 Title = "Музыкальная импровизация",
                 Description = "Импровизация любителей музыки! Создайте шедевр со случайными людьми.",
-                Date = "29 JAN 2026",
+                DateDay = "29 JAN",
+                DateYear = "2026",
                 Time = "22:00"
             });
             hobbyViewModels.Add(new EventWorkshopEventInfoViewModel
@@ -81,7 +62,8 @@ namespace WebNet23Online.Controllers
                 Url = "/images/event-workshop/creation/creation2.jpg",
                 Title = "Чайная церемония",
                 Description = "Примите участие в чайной церемонии! Знакомства, дружеское общение и многое другое.",
-                Date = "17 APR 2026",
+                DateDay = "17 APR",
+                DateYear = "2026",
                 Time = "19:00"
             });
             hobbyViewModels.Add(new EventWorkshopEventInfoViewModel
@@ -89,7 +71,8 @@ namespace WebNet23Online.Controllers
                 Url = "/images/event-workshop/creation/creation3.jpg",
                 Title = "Книжные дебаты",
                 Description = "Читаем, обсуждаем, спорим в поисках истины. Приглашаются все желающие!",
-                Date = "30 MAY 2026",
+                DateDay = "30 MAY",
+                DateYear = "2026",
                 Time = "08:00"
             });
 
@@ -106,7 +89,8 @@ namespace WebNet23Online.Controllers
                 Description = "Неспешно прокатитесь по велосипедным тропам, " +
                 "погружаясь в атмосферу загородной природы, хорошей компании и полезного спорта! " +
                 "Приглашаются все желающие.",
-                Date = "19 JUN 2026",
+                DateDay = "19 JUN",
+                DateYear = "2026",
                 Time = "15:00"
             });
 
@@ -122,7 +106,8 @@ namespace WebNet23Online.Controllers
                 Title = "D&D",
                 Description = "Ограбьте бандитов, сожгите дракона, своруйте золото. " +
                 "Почувствуйте себя гномом в атмосферном приключении мира Dungeon & Dragons! Еда за счёт мастера.",
-                Date = "14 JUL 2026",
+                DateDay = "14 JUL",
+                DateYear = "2026",
                 Time = "14:00"
             });
 
--- Net23Online/WebNet23Online/Models/EventWorkshop/EventWorkshopEventInfoViewModel.cs
+++ Net23Online/WebNet23Online/Models/EventWorkshop/EventWorkshopEventInfoViewModel.cs
@@ -5,7 +5,8 @@
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Date {  get; set; }
+        public string DateDay {  get; set; }
+        public string DateYear { get; set; }
         public string Time {  get; set; }
     }
 }
