0210e8c20ea3298a48a772b6772d689b6644cf5d
Ира Лаврусик <iriplay1995@gmail.com>
2026-04-01 15:55:06 +0300

    japanese cars - buy/sell

---
+++ Net23Online/WebNet23Online/Controllers/JapaneseDomesticMarketController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNet23Online.Controllers
+{
+    public class JapaneseDomesticMarketController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Catalog()
+        {
+            return View();
+        }
+
+        public IActionResult Journal()
+        {
+            return View();
+        }
+    }
+}
