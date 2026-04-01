e112a6c32fa82e55647b8af2617a711f254baa5d
Geliusan <xa2@tut.by>
2026-04-01 18:20:45 +0300

    Transfer the site to ASP.NET

---
+++ Net23Online/WebNet23Online/Controllers/RockLegendsPortalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNet23Online.Controllers
+{
+    public class RockLegendsPortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
