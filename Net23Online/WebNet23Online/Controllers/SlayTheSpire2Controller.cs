93ad724e66cc5cd2e9186bad7ba74f2963211417
Ketuse <catwithshotgunster34@gmail.com>
2026-04-01 18:38:11 +0300

    Add SlayTheSpire2 Page ASP.NET

---
+++ Net23Online/WebNet23Online/Controllers/SlayTheSpire2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNet23Online.Controllers
+{
+    public class SlayTheSpire2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
