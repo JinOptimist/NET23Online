b4cd3c80361c990c590177a23202616a6e2c688a
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-04-04 12:23:05 +0300

    Add FoodItem and new DelightBistroController with HTML

--- Net23Online/WebNet23Online/Controllers/DelightBistro.cs
+++ Net23Online/WebNet23Online/Controllers/DelightBistro.cs
@@ -8,24 +8,178 @@ namespace WebNet23Online.Controllers
         //          /DelightBistro/Index
         public IActionResult Index(string menuType)
         {
-
-            var viewModel = new MenuTypeViewModel() { };
-            switch (menuType)
+            var allFoods = GetAllFoodItems();
+            //var viewModel = new MenuTypeViewModel() { };
+            var viewModels = new List<MenuTypeViewModel>
             {
-                case "soups":
-                    viewModel.MenuType = "Супы";
-                    break;
-                case "hot":
-                    viewModel.MenuType = "Горячее";
-                    break;
-                case "salads":
-                    viewModel.MenuType = "Салаты";
-                    break;
-                default:
-                    break;
-            }
+                new MenuTypeViewModel()
+                {
+                    MenuType="soups",
+                    TypeName="Супы",
+                    FoodItems =allFoods.Where(x=>x.MenuType=="soups").ToList(),
+                },
+                new MenuTypeViewModel()
+                {
+                    MenuType="hot",
+                    TypeName="Горячее",
 
+                    FoodItems =allFoods.Where(x=>x.MenuType=="hot").ToList(),
+                },
+                new MenuTypeViewModel()
+                {
+                    MenuType="salads",
+                    TypeName="Салаты",
+                    FoodItems =allFoods.Where(x=>x.MenuType=="salads").ToList(),
+                }
+            };
+
+            var viewModel = viewModels.Where(x => x.MenuType == menuType).ToList();
+            if (string.IsNullOrEmpty(menuType))
+            {
+                return View(viewModels);
+            }
             return View(viewModel);
+        }
+
+        public List<FoodItem> GetAllFoodItems()
+        {
+
+            return new List<FoodItem>
+             {
+                new FoodItem
+                 {
+                    Name = "Tom Yum Talay",
+                    Price = 20,
+                    ImgURL = "/images/delight-bistro/TomYumTalay.jpg",
+                    MenuType = "soups",
+                    Ingredients = new List<string>
+                    {
+                        "Креветки",
+                        "Шампиньоны",
+                        "Лайм",
+                        "Паста",
+                        "Помидоры",
+                        "Перец чили"
+                    }
+                 },
+
+                new FoodItem
+                {
+                    Name = "Lentil Soup",
+                    Price = 15,
+                    ImgURL = "/images/delight-bistro/LentilSoup.jpg",
+                    MenuType = "soups",
+                    Ingredients = new List<string>
+                   {
+                        "Говядина",
+                        "Чечевица красная",
+                        "Картофель",
+                        "Лук репчатый",
+                        "Помидоры",
+                        "Морковь"
+                   }
+                },
+
+                new FoodItem
+                {
+                    Name = "Стейк с пастой",
+                    Price = 30,
+                    ImgURL = "/images/delight-bistro/Steak2.jpg",
+                    MenuType = "hot",
+                    Ingredients = new List<string>
+                    {
+                        "Говяжий стейк",
+                        "Паста",
+                        "Сливочно‑перечный соус",
+                        "Зелень"
+                    }
+                },
+
+                new FoodItem
+                {
+                    Name = "Лазанья",
+                    Price = 30,
+                    ImgURL = "/images/delight-bistro/Lasana.jpg",
+                    MenuType = "hot",
+                    Ingredients = new List<string>
+                    {
+                        "Паста Лазанья",
+                        "Фарш говяжий",
+                        "Соус бешамель",
+                        "Сыр пармезан",
+                        "Помидоры",
+                        "Зелень"
+                    }
+                },
+
+                new FoodItem
+                {
+                    Name = "Хрустящая курица с кунжутом",
+                    Price = 25,
+                    ImgURL = "/images/delight-bistro/CrispySesameChicken.jpg",
+                    MenuType = "hot",
+                    Ingredients = new List<string>
+                    {
+                        "Курица",
+                        "Семена кунжута",
+                        "Мёд",
+                        "Рис",
+                        "Лук репчатый",
+                        "Чесночная паста",
+                        "Белый перец"
+                    }
+                },
+
+                new FoodItem
+                {
+                    Name = "Паста с креветками, чесноком и чили",
+                    Price = 25,
+                    ImgURL = "/images/delight-bistro/ChilliGarlicPrawnPasta.jpg",
+                    MenuType = "hot",
+                    Ingredients = new List<string>
+                    {
+                        "Креветки",
+                        "Лингвини",
+                        "Помидоры черри",
+                        "Хлопья чили",
+                        "Зелень"
+                    }
+                },
+
+                new FoodItem
+                {
+                    Name = "Салат Цезарь",
+                    Price = 20,
+                    ImgURL = "/images/delight-bistro/CesarSalad.jpg",
+                    MenuType = "salads",
+                    Ingredients = new List<string>
+                    {
+                        "Римский салат",
+                        "Сухарики",
+                        "Пармезан",
+                        "Чёрный молотый перец",
+                        "Курица",
+                        "Морковь"
+                    }
+                },
+
+                new FoodItem
+                {
+                    Name = "Салат средиземноморский",
+                    Price = 25,
+                    ImgURL = "/images/delight-bistro/Salad2.jpg",
+                    MenuType = "salads",
+                    Ingredients = new List<string>
+                    {
+                        "Микс салатов",
+                        "Помидоры черри",
+                        "Чёрные оливки",
+                        "Сыр",
+                        "Чёрный молотый перец",
+                        "Оливковое масло"
+                    }
+                }
+            };
         }
     }
 }
---
+++ Net23Online/WebNet23Online/Models/DelightBistro/FoodItem.cs
@@ -0,0 +1,11 @@
+﻿namespace WebNet23Online.Models.DelightBistro
+{
+    public class FoodItem
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string ImgURL { get; set; }
+        public string MenuType { get; set; }
+        public List<string> Ingredients { get; set; }
+    }
+}
