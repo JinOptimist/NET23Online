efe4745dafc63f50d63f16db65dd4b298eb4146e
Марта Локить <lokit2002@mail.ru>
2026-04-06 14:28:33 +0300

    Add Post

---
+++ Net23Online/WebNet23Online/Controllers/MaksKorzController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebNet23Online.Models.Maks_Korz;
+
+namespace WebNet23Online.Controllers
+{
+    public class MaksKorzController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string LastName, int Age, string Country)
+        {
+            DataUser user = new DataUser();
+            StatusUser status = new StatusUser();
+            user.LastName = LastName;
+            user.Age = Age;
+            user.Country = Country;
+            status.User = user;
+            if (Age < 18)
+            {
+                status.Status = "Не совершенолетний!";
+            }
+            else
+            {
+                status.Status = "Cовершенолетний!";
+            }
+            return View("/Views/MaksKorz/FormUser.cshtml");
+        }
+    }
+}
