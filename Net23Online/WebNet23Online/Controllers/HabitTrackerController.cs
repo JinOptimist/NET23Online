7b1f42e81dcd77566a3db2d16c94217d73393c91
Соня Федоренко <sonyfedorenko@gmail.com>
2026-04-09 18:26:27 +0300

    Create Controller and Layout, HttpGet and HttpPost

--- Net23Online/WebNet23Online/Controllers/HabitTrackerController.cs
+++ Net23Online/WebNet23Online/Controllers/HabitTrackerController.cs
@@ -1,21 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebNet23Online.Models.HabitTracker;
 
 namespace WebNet23Online.Controllers;
 
 public class HabitTrackerController : Controller
 {
+    //статик это плохо
+    private static HabitTrackerViewModel _habitsTraker  = new HabitTrackerViewModel();
+    
+    private static List<HabitViewModel> _allHabits = new List<HabitViewModel>
+    {
+        new HabitViewModel { Title = "Спорт 30 мин", ColorOfDot = "pink" },
+        new HabitViewModel { Title = "Английский 20 мин", ColorOfDot = "blue"},
+        new HabitViewModel { Title = "Программирование 1 час",  ColorOfDot = "green"},
+        new HabitViewModel { Title = "Вода 2л", ColorOfDot = "purple"},
+    };
+    
+    
     public IActionResult Index()
     {
-        return View();
+        var model = _habitsTraker;
+        model.Habits = _allHabits;
+        return View(model);
     } 
     
+    [HttpGet]
     public IActionResult Statistics()
     {
-        return View();
+        var  model = _habitsTraker;
+        model.Habits = _allHabits;
+
+        foreach (var habit in _allHabits)
+        {
+            var doneCount = habit.WeekResults.Count(x => x);
+            var percent = (float)doneCount / 7 * 100;
+            
+            habit.DoneCount = doneCount;
+            habit.Percent = percent;
+        }
+        return View(model);
     } 
     
+    [HttpGet]
     public IActionResult Diary()
     {
         return View();
     } 
+    
+    [HttpGet]
+    public IActionResult CreateHabit()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult CreateHabit(HabitViewModel  habit)
+    {
+        if (string.IsNullOrEmpty(habit.Title) 
+            || _allHabits.Any(h => h.Title == habit.Title))
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        
+        var availibleColors = new[] { "pink", "blue", "green", "purple" };
+        
+        var indexOfColorForHabit = _allHabits.Count % availibleColors.Length; 
+        
+        habit.ColorOfDot = availibleColors[indexOfColorForHabit];
+        _allHabits.Add(habit);
+        
+        return RedirectToAction(nameof(Index));
+    }
 }
