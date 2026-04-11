96d7a1033be26cbd8abc99b2d2241099edd3fcb6
Денис Кибицкий <aidns410@gmail.com>
2026-04-11 03:48:17 +0300

    fix mistakes

--- Net23Online/WebNet23Online/Controllers/RockBandsController.cs
+++ Net23Online/WebNet23Online/Controllers/RockBandsController.cs
@@ -8,20 +8,16 @@ namespace WebNet23Online.Controllers
     public class RockBandsController : Controller
     {
         private readonly IRockBandsService _rockBandsService;
-        private WebContext _webContext;
 
         public RockBandsController(IRockBandsService rockBandsService, WebContext webContext)
         {
             _rockBandsService = rockBandsService;
-            _webContext = webContext;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var viewAllRockBandsDataFromDatabase = _webContext.RockBand.ToList();
             var viewModel = new RockBandsIndexViewModel { Bands = _rockBandsService.GetBands() };
-            //var viewModel = new RockBandsIndexViewModel { Bands = _rockBandsService.GetBands() };
             return View(viewModel);
         }
 
