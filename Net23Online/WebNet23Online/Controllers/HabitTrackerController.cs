e7ac48c4c88dc405fff937a4871baf1c2ee5facc
Соня Федоренко <sonyfedorenko@gmail.com>
2026-04-13 19:12:53 +0300

    Change Statistics()

--- Net23Online/WebNet23Online/Controllers/HabitTrackerController.cs
+++ Net23Online/WebNet23Online/Controllers/HabitTrackerController.cs
@@ -37,6 +37,7 @@ public class HabitTrackerController : Controller
             .FirstOrDefault();
 
         var model = _habitService.GenerateHabitTracker(habitTrackerData);
+        _statisticsService.CreateStatisticsInfo(model);
         
         return View(model);
     } 
