c624020e855c6b58826ac3874a86d955161bfa02
Соня Федоренко <sonyfedorenko@gmail.com>
2026-04-13 22:19:06 +0300

    Fix Program.cs

--- Net23Online/WebNet23Online/Program.cs
+++ Net23Online/WebNet23Online/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddDbContext<WebContext>(op => op.UseSqlServer(connectionString
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<ILittleLemonMenuService, LittleLemonMenuService>();
+builder.Services.AddScoped<ILittleLemonTestimonialService, LittleLemonTestimonialService>();
+builder.Services.AddScoped<ILittleLemonSubscribeService, LittleLemonSubscribeService>();
 // Register Services
 //builder.Services.AddScoped<IAnimeGirlGenerator, AnimeGirlGenerator>(diContainer =>
 //{
