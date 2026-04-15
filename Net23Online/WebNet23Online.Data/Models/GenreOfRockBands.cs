bbd05e2217bdbf8bf692dce5873f25fcbcbac111
Денис Кибицкий <aidns410@gmail.com>
2026-04-15 22:15:53 +0300

    final 1 step fix

--- Net23Online/WebNet23Online.Data/Models/GenreOfRockBands.cs
+++ Net23Online/WebNet23Online.Data/Models/GenreOfRockBands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace WebNet23Online.Data.Models
 {
     public class GenreOfRockBands : BaseModel
     {
-        public string Name { get; set; }
-        public ICollection<RockBandGenreData> RockBandGenres { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<RockBandGenreData> RockBandGenres { get; set; } = new List<RockBandGenreData>();
     }   
 }
