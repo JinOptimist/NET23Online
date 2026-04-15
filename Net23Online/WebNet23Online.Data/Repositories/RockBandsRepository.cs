b2defc8a6376a3478db8d77ef000bb6273e412bc
Денис Кибицкий <aidns410@gmail.com>
2026-04-15 13:53:31 +0300

    add method add

--- Net23Online/WebNet23Online.Data/Repositories/RockBandsRepository.cs
+++ Net23Online/WebNet23Online.Data/Repositories/RockBandsRepository.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebNet23Online.Data.Models;
 
 namespace WebNet23Online.Data.Repositories
@@ -10,14 +7,17 @@ namespace WebNet23Online.Data.Repositories
     public class RockBandsRepository : BaseRepository<RockBandsData>
     {
         public RockBandsRepository(WebContext webContext) : base(webContext) { }
-    }
-     public override void Add(RockBandsData model)
+
+        public override void Add(RockBandsData model)
         {
-            if (model.Name == model.Description)
+            var normalizedName = model.Name?.Trim().ToLower();
+            if (_dbSet.Any(x => x.Name.ToLower() == normalizedName))
             {
-                throw new Exception("Be more creative");
+                throw new InvalidOperationException($"Rock band with name '{normalizedName}' already exists.");
             }
 
+            model.Name = normalizedName;
             base.Add(model);
         }
     }
+}
