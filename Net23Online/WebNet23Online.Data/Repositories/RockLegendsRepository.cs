d91ae5e7d8255c446bfd3509c4260d1931d4d0fb
Geliusan <xa2@tut.by>
2026-04-15 21:17:05 +0300

    Many changes

---
+++ Net23Online/WebNet23Online.Data/Repositories/RockLegendsRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebNet23Online.Data.Models;
+using WebNet23Online.Data.Repositories.Interfaces;
+
+namespace WebNet23Online.Data.Repositories
+{
+    public class RockLegendsRepository : BaseRepository<RockLegendsData>, IRockLegendsRepository
+    {
+        public RockLegendsRepository(WebContext webContext) : base(webContext) { }
+
+        public RockLegendsData? GetById(int id)
+        {
+            return _dbSet.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(RockLegendsData model)
+        {
+            _context.Update(model);
+            _context.SaveChanges();
+        }
+    }
+}
