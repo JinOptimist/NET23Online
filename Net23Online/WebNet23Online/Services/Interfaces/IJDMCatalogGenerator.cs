a7cbea7ba27090464fe978ad3c386fe1a48c3392
Ира Лаврусик <iriplay1995@gmail.com>
2026-04-15 14:26:24 +0300

    added services, post/get requests for JDM

---
+++ Net23Online/WebNet23Online/Services/Interfaces/IJDMCatalogGenerator.cs
@@ -0,0 +1,9 @@
+﻿using WebNet23Online.Models.JapaneseDomesticMarket;
+
+namespace WebNet23Online.Services.Interfaces
+{
+    public interface IJDMCatalogGenerator
+    {
+        List<JDMCatalogViewModels> GetManufacturerTypeFromJDMItems(List<JapaneseDomesticMarketViewModels> carsJDMItems, string sortMenuType);
+    }
+}
