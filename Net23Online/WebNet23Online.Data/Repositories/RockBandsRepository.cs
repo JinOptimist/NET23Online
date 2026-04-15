b0521d02b5025e89ee24975ceb64f690f4dac5ef
Денис Кибицкий <aidns410@gmail.com>
2026-04-15 14:21:36 +0300

    Pull req

--- Net23Online/WebNet23Online.Data/Repositories/RockBandsRepository.cs
+++ Net23Online/WebNet23Online.Data/Repositories/RockBandsRepository.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
+﻿using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 using WebNet23Online.Data.Models;
 using WebNet23Online.Data.Repositories.Interfaces;
