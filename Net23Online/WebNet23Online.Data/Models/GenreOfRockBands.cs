77e8a099e672435f8febfa3fc508055a37044d6e
Денис Кибицкий <aidns410@gmail.com>
2026-04-15 22:09:15 +0300

    first step. add db models

---
+++ Net23Online/WebNet23Online.Data/Models/GenreOfRockBands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebNet23Online.Data.Models
+{
+    public class GenreOfRockBands : BaseModel
+    {
+        public string Name { get; set; }
+        public ICollection<RockBandGenreData> rockBandGenres { get; set; }
+    }   
+}
---
+++ Net23Online/WebNet23Online.Data/Models/RockBandGenreData.cs
@@ -0,0 +1,10 @@
+﻿namespace WebNet23Online.Data.Models
+{
+    public class RockBandGenreData
+    {
+        public int RockBandId { get; set; }
+        public RockBandsData RockBand { get; set; } = null!;
+        public int genreId { get; set; }
+        public GenreOfRockBands Genre { get; set; } = null!;
+    }
+}
