6f8b40b222e48985c63ad6c516d20450c99e3faa
Соня Федоренко <sonyfedorenko@gmail.com>
2026-04-15 18:48:40 +0300

    fix tabs

--- Net23Online/WebNet23Online.Data/WebContext.cs
+++ Net23Online/WebNet23Online.Data/WebContext.cs
@@ -1,23 +1,23 @@
-﻿        using Microsoft.EntityFrameworkCore;
-        using WebNet23Online.Data.Models;
-        using WebNet23Online.Data.Models.Steam;
+﻿using Microsoft.EntityFrameworkCore;
+using WebNet23Online.Data.Models;
+using WebNet23Online.Data.Models.Steam;
 
-        namespace WebNet23Online.Data
-        {
-            public class WebContext : DbContext
-            {
-                public DbSet<AnimeGirlData> AnimeGirls { get; set; }
-                public DbSet<UserData> Users { get; set; }
-                public DbSet<MazeData> Mazes { get; set; }
-                public DbSet<HabitTrackerData> HabitTracker { get; set; }
-                //public DbSet<HabitData> Habit { get; set; }
-                public DbSet<BeastData> Beasts { get; set; }
-                public DbSet<RockBandsData> RockBand { get; set; }
-                public DbSet<FoodItemData> FoodItems { get; set; }
+namespace WebNet23Online.Data
+{
+    public class WebContext : DbContext
+    {
+        public DbSet<AnimeGirlData> AnimeGirls { get; set; }
+        public DbSet<UserData> Users { get; set; }
+        public DbSet<MazeData> Mazes { get; set; }
+        public DbSet<HabitTrackerData> HabitTracker { get; set; }
+        //public DbSet<HabitData> Habit { get; set; }
+        public DbSet<BeastData> Beasts { get; set; }
+        public DbSet<RockBandsData> RockBand { get; set; }
+        public DbSet<FoodItemData> FoodItems { get; set; }
 
-                public DbSet<RockLegendsData> RockLegends { get; set; }
-                public DbSet<GameData> Games { get; set; }
+        public DbSet<RockLegendsData> RockLegends { get; set; }
+        public DbSet<GameData> Games { get; set; }
 
-                public WebContext(DbContextOptions<WebContext> options) : base(options) { }
-            }
-        }
+        public WebContext(DbContextOptions<WebContext> options) : base(options) { }
+    }
+}
