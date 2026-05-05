0748e8de32bdc7be5f8b4a7241d16e9828efdbaf
Денис Кибицкий <aidns410@gmail.com>
2026-05-05 17:35:00 +0300

    done

--- Net23Online/WebNet23Online/Controllers/RockBandsController.cs
+++ Net23Online/WebNet23Online/Controllers/RockBandsController.cs
@@ -43,9 +43,14 @@ namespace WebNet23Online.Controllers
 
         [HttpPost]
         [Authorize]
-        public IActionResult Index(RockBandsIndexViewModel viewModel)
+        public IActionResult Index(RockBandsIndexViewModel viewModel, IFormFile? Image)
         {
             var band = viewModel.BandBlock;
+            if (Image != null)
+            {
+                band.PhotoOfTheBand = Image;
+                ModelState.Remove("BandBlock.ImageUrl");
+            }
             if (!ModelState.IsValid)
             {
                 var genres = _rockBandsService.GetGenres();
