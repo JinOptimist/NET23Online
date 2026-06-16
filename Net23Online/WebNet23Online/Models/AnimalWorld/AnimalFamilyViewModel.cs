0e441a4ab99ec3c77b3722380969a6dcff214b6b
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-06-16 21:14:39 +0300

    Unique name attribute with reflections.

--- Net23Online/WebNet23Online/Models/AnimalWorld/AnimalFamilyViewModel.cs
+++ Net23Online/WebNet23Online/Models/AnimalWorld/AnimalFamilyViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using WebNet23Online.Data.Repositories.Interfaces.AnimalWorld;
 using WebNet23Online.Models.CustomValidatioAttributes.AnimalWorld;
 
 namespace WebNet23Online.Models.AnimalWorld
@@ -6,7 +7,7 @@ namespace WebNet23Online.Models.AnimalWorld
     public class AnimalFamilyViewModel
     {
         [Required]
-        [AnimalFamilyUniqueName]
+        [UniqueName(typeof(IAnimalFamilyRepository))]
         public string AnimalFamilyName { get; set; }
 
         [Required]
---
+++ Net23Online/WebNet23Online/Models/CustomValidatioAttributes/AnimalWorld/UniqueNameAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace WebNet23Online.Models.CustomValidatioAttributes.AnimalWorld
+{
+    public class UniqueNameAttribute : ValidationAttribute
+    {
+        private Type _repositoryType;
+        private const string METHOD_NAME = "GetElementByName";
+
+        public UniqueNameAttribute(Type repositoryType)
+        {
+            _repositoryType = repositoryType;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var name = value as string;
+            var repository = validationContext.GetRequiredService(_repositoryType);
+            var method = _repositoryType.GetMethod(METHOD_NAME);
+            if (method == null)
+            {
+                foreach (var interfaceType in _repositoryType.GetInterfaces())
+                {
+                    method = interfaceType.GetMethod(METHOD_NAME);
+                    if (method != null)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (method == null)
+            {
+                return new ValidationResult("Отсутствует метод");
+            }
+
+            var result = method.Invoke(repository, new object[] { name });
+            if (result != null)
+            {
+                return new ValidationResult("Такое имя уже используется");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
